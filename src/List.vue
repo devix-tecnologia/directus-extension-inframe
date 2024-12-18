1a9898d0b0cedac99363cbe7342da4e81f869cb2
Fernando Gatti <contato@fernandogatti.com>
2024-12-18 16:01:38 -0300

    feat: Implementa thumbnail

--- src/List.vue
+++ src/List.vue
@@ -8,10 +8,13 @@
       <!-- Exibe os cards com os itens -->
       <div class="card-container">
         <div v-for="item in items" :key="item.id" class="card">
-          <router-link :to="`/inframe/${item.id}`" class="card-link">
+          <router-link
+            :to="`/inframe/${item.id}`"
+            class="card-link"
+            :style="{ backgroundImage: `url('/assets/${item.thumbnail}')` }"
+          >
             <div class="card-header">
               <h3>{{ getTitle(item.translations) }}</h3>
-              <!-- Exibindo a thumbnail se disponível -->
             </div>
           </router-link>
         </div>
@@ -52,7 +55,7 @@ export default defineComponent({
   display: flex;
   flex-wrap: wrap;
   gap: 20px;
-  padding: 20px;
+  padding: 20px 50px;
 }
 
 .card-container {
@@ -67,10 +70,11 @@ export default defineComponent({
   border-radius: 8px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
   flex: 1 1 25%;
-  height: 150px;
-  display: flex; /* Torna o container um flexbox */
-  justify-content: center; /* Alinha o conteúdo horizontalmente */
-  align-items: center; /* Alinha o conteúdo verticalmente */
+  height: 200px;
+  display: flex;
+  flex-direction: column; /* Para garantir que o conteúdo não sobreponha a imagem */
+  justify-content: flex-end; /* Ajusta o conteúdo para o fundo */
+  align-items: center;
   text-align: center;
   overflow: hidden;
   transition: transform 0.3s ease, box-shadow 0.3s ease;
@@ -82,17 +86,33 @@ export default defineComponent({
 }
 
 .card-link {
-  display: block;
-  text-decoration: none;
-  color: inherit;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end; /* Coloca o conteúdo no fundo do link */
+  align-items: center;
+  height: 100%;
+  text-decoration: none; /* Remove o estilo de link */
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  padding: 0;
+  width: 100%;
+}
+
+.card-header h3 {
+  margin: 0; /* Remove a margem para melhor controle de espaçamento */
+  padding: 10px;
+  background: var(--theme--primary);
+  width: 100%;
 }
 
 .card-header {
   background-color: var(--theme--primary-light);
-  padding: 16px;
+  padding: 0;
   font-size: 18px;
   font-weight: bold;
-  color: var(--theme--primary);
+  color: var(--theme--foreground);
+  width: 100%;
 }
 
 .card-body {
@@ -103,11 +123,5 @@ export default defineComponent({
 
 .card-body p {
   margin: 0;
-}
-
-.thumbnail {
-  width: 100%;
-  height: auto;
-  max-width: 200px;
 }
 </style>
